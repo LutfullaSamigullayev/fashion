2b1beb7cde9c224020899abc62da8dd2aedad546
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-05-01 12:41:42 +0500

    add Banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,7 +1,14 @@
 export const Banner = () => {
     return (
         <div className="h-[712px] bg-[url(/banner/banner.jpg)] bg-cover bg-center">
-            <div className="container_"><p>Elevate Your</p></div>
+            <div className="container_ py-16 px-8 h-full flex flex-col justify-end gap-y-5">
+                <div  className="text-2xl">
+                <p>Elevate Your Style</p>
+                <p>Timeless Fashion, Sustainable</p>
+                <p>Choices</p>
+                </div>
+                <button className="w-fit py-3 px-5 bg-white text-black">Shop Now</button>
+                </div>
         </div>
     )
 }
