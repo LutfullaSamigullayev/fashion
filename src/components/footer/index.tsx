ee4304abe51a95d542561219cc76f3eff30f2e57
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-05-02 11:46:59 +0500

    footer complate

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,16 +4,40 @@ import { FooterMenu } from "./components";
 export const Footer = () => {
   return (
     <div className="bg-[#F5F4F4] text-black">
-      <div className="container_ flex justify-between">
-        <div className="w-[576px] flex gap-x-6 py-20">
-          {footerItems.map(item => (
-            <FooterMenu key={item.id} title={item.title} data={item.data} />
-          ))}
-         
+      <div className="container_ ">
+        <div className="flex justify-between py-20">
+          <div className="w-[576px] flex gap-x-6 ">
+            {footerItems.map((item) => (
+              <FooterMenu key={item.id} title={item.title} data={item.data} />
+            ))}
+          </div>
+          <div className="w-[490px] flex flex-col gap-y-6 px-6">
+            <h3 className="text-2xl">Get the latest new from us</h3>
+            <div>
+              <div className="pb-4">
+                <input
+                  type="text"
+                  placeholder="Enter your email address"
+                  className="w-full py-3 px-4 border border-[#D3D4D5]"
+                />
+                <p className="py-1">
+                  Bu signing up, you agree to our{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Privacy Policy
+                  </a>{" "}
+                  and{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Terms of Service.
+                  </a>
+                </p>
+              </div>
+              <button className="w-fit py-3 px-5 bg-black text-white">
+                Subcribe
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="w-[490px]">
-          
-        </div>
+        <p className="text-[#565656] pb-11">&copy;CEIN</p>
       </div>
     </div>
   );
